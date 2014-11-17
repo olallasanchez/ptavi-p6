c3cdb99717e844f9acf716552b0623e86acb0c63
olalla sanchez-valladares pedraza <opedraza@iota31.aulas.gsyc.es>
2014-11-17 10:45:47 +0100

    Servidor sin terminar, ni RTP

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import SocketServer
+import sys
 
 
 class EchoHandler(SocketServer.DatagramRequestHandler):
@@ -14,18 +15,29 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write("Hemos recibido tu peticion")
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
-            print "El cliente nos manda " + line
-
-            # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
+            print line
+            Info = line.split(' ')
+            Metodo = Info[0]
+            if Metodo == 'INVITE':
+                Envio = 'SIP/2.0 100 Trying\r\n\r\n'
+                Envio += 'SIP/2.0 180 Ring\r\n\r\n'
+                Envio += 'SIP/2.0 200 OK\r\n\r\n'
+                self.wfile.write(Envio)
+            elif Metodo == 'ACK':
+                print 'OLALLA ACK'
+            elif Metodo == 'BYE':
+                print 'OLALLA BYE'
+
+            # Si no hay más líneas salimos del bucle infinito
+            
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    serv = SocketServer.UDPServer(("", 6001), EchoHandler)
-    print "Lanzando servidor UDP de eco..."
+    serv = SocketServer.UDPServer(("", int(sys.argv[2])), EchoHandler)
+    print "Listening..."
     serv.serve_forever()
