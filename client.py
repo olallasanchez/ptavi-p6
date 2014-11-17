cd8faa9368ec4600e7c307af9de704cedf045378
olalla sanchez-valladares pedraza <opedraza@zeta24.aulas.gsyc.es>
2014-11-17 13:14:34 +0100

    Cliente sin terminar, ni RTP

--- client.py
+++ client.py
@@ -8,35 +8,40 @@ import socket
 import sys
 
 # Cliente UDP simple.
+if len(sys.argv) == 3:
+    IP_Puerto = sys.argv[2].split(':')
+    Correo = IP_Puerto[0]
+    Puerto = IP_Puerto[1]
+    Correo_Trocea = Correo.split('@')
+    Metodo = sys.argv[1].upper()
 
-IP_Puerto = sys.argv[2].split(':')
-Correo = IP_Puerto[0]
-Puerto = IP_Puerto[1]
-Correo_Trocea = Correo.split('@')
-Metodo = sys.argv[1].upper()
+    # Dirección IP del servidor.
+    SERVER = Correo_Trocea[1]
+    PORT = int(IP_Puerto[1])
 
-# Dirección IP del servidor.
-SERVER = Correo_Trocea[1]
-PORT = int(IP_Puerto[1])
+    # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+    my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    my_socket.connect((SERVER, PORT))
 
-# Contenido que vamos a enviar
-LINE = Metodo + ' sip:' + IP_Puerto[0] + ' SIP/2.0\r\n\r\n'
+    # Contenido que vamos a enviar
+    try:
+        LINE = Metodo + ' sip:' + IP_Puerto[0] + ' SIP/2.0\r\n\r\n'
+        my_socket.send(LINE)
+        data = my_socket.recv(1024)
+        print "Enviando: " + LINE
+    except socket.error:
+        print 'Error: No server listening at ' + Correo_Trocea[1] + ' port ' + IP_Puerto[1]
+        raise SystemExit
 
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((SERVER, PORT))
 
-print "Enviando: " + LINE
-my_socket.send(LINE)
-data = my_socket.recv(1024)
+    print 'Recibido -- ', data
+    if Metodo == 'INVITE':
+        LINE = 'ACK sip:' + IP_Puerto[0] + ' SIP/2.0\r\n\r\n'
+        my_socket.send(LINE)
 
-print 'Recibido -- ', data
-if Metodo == 'INVITE':
-    LINE = 'ACK sip:' + IP_Puerto[0] + ' SIP/2.0\r\n\r\n'
-
-my_socket.send(LINE)
-
-# Cerramos todo
-my_socket.close()
-print "Fin."
+    # Cerramos todo
+    my_socket.close()
+    print "Fin."
+else:
+    print 'Usage: python client.py method receiver@IP:SIPport'
