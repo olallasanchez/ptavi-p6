977f63a55d697744fa8274c230de9a9016a9083e
olalla sanchez-valladares pedraza <opedraza@iota31.aulas.gsyc.es>
2014-11-17 10:45:24 +0100

    Cliente sin terminar, ni RTP

--- client.py
+++ client.py
@@ -5,15 +5,22 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys
 
 # Cliente UDP simple.
 
+IP_Puerto = sys.argv[2].split(':')
+Correo = IP_Puerto[0]
+Puerto = IP_Puerto[1]
+Correo_Trocea = Correo.split('@')
+Metodo = sys.argv[1].upper()
+
 # Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+SERVER = Correo_Trocea[1]
+PORT = int(IP_Puerto[1])
 
 # Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+LINE = Metodo + ' sip:' + IP_Puerto[0] + ' SIP/2.0\r\n\r\n'
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -21,11 +28,14 @@ my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
 print "Enviando: " + LINE
-my_socket.send(LINE + '\r\n')
+my_socket.send(LINE)
 data = my_socket.recv(1024)
 
 print 'Recibido -- ', data
-print "Terminando socket..."
+if Metodo == 'INVITE':
+    LINE = 'ACK sip:' + IP_Puerto[0] + ' SIP/2.0\r\n\r\n'
+
+my_socket.send(LINE)
 
 # Cerramos todo
 my_socket.close()
